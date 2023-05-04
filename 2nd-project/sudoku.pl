5eb1910dfe718286983b1113b88bc5e05c39a81e
Nyck <nyckbrandao1236@gmail.com>
2023-05-04 20:53:34 +0100

    Changed/Fixed bug - empty square now maintains _ (empty), instead of making it be the character '_'.

--- 2nd-project/sudoku.pl
+++ 2nd-project/sudoku.pl
@@ -107,7 +107,7 @@ print_puzzle_r([H|T], N) :-
 % Prints a row of the puzzle
 print_row([], _).
 print_row([H|T], N) :-
-    (H = '_' -> write(' '); write(H)),
+    (nonvar(H) -> write(H); write(' ')),
     ((N == 3; N == 6) 
         -> write(' | ')
         ;  write(' ')
@@ -138,6 +138,20 @@ goal(Puzzle) :-
     rows_valid(Puzzle),
     columns_valid(Puzzle),
     subgrids_valid(Puzzle).
+
+%goal(Puzzle) :-
+%        length(Puzzle, 9), maplist(same_length(Puzzle), Puzzle),
+%        append(Puzzle, Vs), Vs ins 1..9,
+%        maplist(all_distinct, Puzzle),
+%        transpose(Puzzle, Columns),
+%        maplist(all_distinct, Columns),
+%        Puzzle = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
+%        blocks(As, Bs, Cs), blocks(Ds, Es, Fs), blocks(Gs, Hs, Is).
+
+%blocks([], [], []).
+%blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
+ %       all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
+  %      blocks(Ns1, Ns2, Ns3).
 
 rows_valid([]).
 rows_valid([Row|Rows]) :-
